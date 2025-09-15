c10be97b7d21de376636b90d7e08753549d63d15
artem <subotamaxstudy@gmail.com>
2025-09-15 10:03:00 +0300

    messege

---
+++ Lesson 2/Варіант8 5.py
@@ -0,0 +1,3 @@
+import mathnumber =384726
+max_digit= max(int(d) for d in str(number))
+print("5.Найбільша цифра у числі:", max_digit)
---
+++ Lesson 2/Варіант8 6.py
@@ -0,0 +1,3 @@
+import random
+arr=[random.randint(0,50) for_in range(10)]
+print("6.Масив:", arr)
